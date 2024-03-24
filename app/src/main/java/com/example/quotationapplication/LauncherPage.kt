7c19a5aff303d20f1ac845c7fb7cd6c75c7322b0
PhilippKroger <max.bondarev2005@gmail.com>
2024-03-24 22:06:34 +0300

    removed unused imports

--- app/src/main/java/com/example/quotationapplication/LauncherPage.kt
+++ app/src/main/java/com/example/quotationapplication/LauncherPage.kt
@@ -1,23 +1,11 @@
 package com.example.quotationapplication
 
-import android.graphics.drawable.Icon
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.ElevatedButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
--- app/src/main/java/com/example/quotationapplication/LoginPage.kt
+++ app/src/main/java/com/example/quotationapplication/LoginPage.kt
@@ -1,11 +1,11 @@
 package com.example.quotationapplication
+
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
