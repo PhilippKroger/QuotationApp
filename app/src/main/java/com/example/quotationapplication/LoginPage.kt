bf10273a1aa55a50c59b6496a5bd3f47123fa074
PhilippKroger <max.bondarev2005@gmail.com>
2024-03-25 11:48:14 +0300

    made a login page

--- app/src/main/java/com/example/quotationapplication/LoginPage.kt
+++ app/src/main/java/com/example/quotationapplication/LoginPage.kt
@@ -5,18 +5,31 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.quotationapplication.R
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 
 @ExperimentalFoundationApi
 @Composable
@@ -63,4 +76,100 @@ fun LoginPage() {
             color = Color(0xFFA7AAAE)
         )
     }
+
+
+    val (emailValue, setEmailValue) = remember { mutableStateOf("") }
+    val (passwordValue, setPasswordValue) = remember { mutableStateOf("") }
+
+    Column {
+        EmailTextField(emailValue = emailValue, onEmailValueChange = setEmailValue)
+        Spacer(modifier = Modifier.height(16.dp))
+        PasswordTextField(passwordValue = passwordValue, onPasswordValueChange = setPasswordValue)
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.BottomCenter
+
+    ) {
+        TextButton( modifier = Modifier
+            .padding(end=180.dp),
+            onClick = { /*TODO*/ },
+
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.back_icon),
+                contentDescription = "",
+                modifier = Modifier
+                    .fillMaxSize(0.1f)
+            )
+            Text( "Назад" , color = Color(0xFFA7AAAE))
+        }
+
+        TextButton( modifier = Modifier
+            .padding(start=180.dp),
+            onClick = { /*TODO*/ },
+        ) {
+
+            Text( "Далее" , color = Color(0xFF283C63))
+            Image(
+                painter = painterResource(id = R.drawable.img_4),
+                contentDescription = "",
+                modifier = Modifier
+                    .fillMaxSize(0.10f)
+            )
+        }
+    }
+
+
 }
+
+
+
+@Composable
+fun EmailTextField(
+    emailValue: String,
+    onEmailValueChange: (String) -> Unit
+) {
+    OutlinedTextField(
+        modifier = Modifier
+            .padding(top = 170.dp)
+            .padding(start = 50.dp),
+        textStyle = MaterialTheme.typography.bodyMedium,
+        value = emailValue,
+        onValueChange = onEmailValueChange,
+        label = { Text("Почта") },
+        keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Email)
+    )
+}
+
+@Composable
+fun PasswordTextField(
+    passwordValue: String,
+    onPasswordValueChange: (String) -> Unit
+) {
+    OutlinedTextField(
+        modifier = Modifier
+            .padding(start=50.dp),
+        value = passwordValue,
+        onValueChange = onPasswordValueChange,
+        label = { Text("Пароль") },
+        textStyle = MaterialTheme.typography.bodyMedium,
+        visualTransformation = PasswordVisualTransformation(),
+        keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Password)
+    )
+}
+
+@Preview
+@Composable
+fun EmailAndPasswordFieldsPreview() {
+    Column {
+        EmailTextField(emailValue = "") { }
+        Spacer(modifier = Modifier.height(16.dp))
+        PasswordTextField(passwordValue = "") { }
+    }
+}
+
+
+
